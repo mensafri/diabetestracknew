1c36d04f98d644b025b770e11ef683fb71d7fc82
mensafri <safrijawad1245@gmail.com>
2023-10-07 11:43:59 +0700

    wip

--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Pasien extends Model
 {
     use HasFactory;
+    protected $guarded = ['id'];
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class, 'dokter_id', 'id');
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
