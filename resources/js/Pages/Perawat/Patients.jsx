f377c1750c9bbb2e9c39ee90feaf967c0030bbc2
mensafri <safrijawad1245@gmail.com>
2023-10-12 20:45:51 +0700

    fix list pasien

--- resources/js/Pages/Perawat/Patients.jsx
+++ resources/js/Pages/Perawat/Patients.jsx
@@ -1,4 +1,5 @@
 
+import ListPasien from "@/Components/ListPasien";
 import SidebarNavPerawat from "@/Components/SidebarNavPerawat";
 import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
@@ -17,7 +18,7 @@ export default function Patient({ auth, list_pasien }) {
             >
                 <Head title="Dashboard" />
                 <div className="bg-blue-300 flex">
-                    <SidebarNavPerawat/>
+                    <SidebarNavPerawat />
                     <div className="mt-20 items-center w-full">
                         <div className=" flex justify-center">
                             <div className="w-5/6 rounded-md ">
@@ -28,20 +29,7 @@ export default function Patient({ auth, list_pasien }) {
                                     </div>
                                 </div>
                                 <div className="bg-gray-100 mt-10 py-8 px-6 rounded-2xl">
-                                    {list_pasien.map(pasien => {
-                                        return (
-                                            <div className="list-pasien flex justify-between bg-white rounded-3xl py-4 px-6">
-                                                <div className="flex">
-                                                    <img className="w-14" src={profile1} alt="" />
-                                                    <div className="ml-6 self-center">
-                                                        <h4 className="text-lg font-semibold">{pasien.nama}</h4>
-                                                        <p className="text-gray-500 text-sm">Dr.<span>{pasien.nama_dokter}</span></p>
-                                                    </div>
-                                                </div>
-                                                <img src={info} alt="" />
-                                            </div>
-                                        )
-                                    })}
+                                    <ListPasien list_pasien={list_pasien} />
                                 </div>
                             </div>
                         </div>
